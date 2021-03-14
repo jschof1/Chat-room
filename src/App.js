17dbc6204f33f5e8759f901620df28b586706c70
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2021-03-14 01:15:16 +0100

    firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.intializeApp({
+  apiKey: "AIzaSyA6jQFKyoxouAhDiMKftcV8rPBYRbQ5vU0",
+  authDomain: "chat-roo-4b80a.firebaseapp.com",
+  projectId: "chat-roo-4b80a",
+  storageBucket: "chat-roo-4b80a.appspot.com",
+  messagingSenderId: "473768943165",
+  appId: "1:473768943165:web:5e437d8bcc8d4fd2ef1454",
+  measurementId: "G-1XG750QWRZ"
+});
+
+const auth = firebase.auth();
+
+const firestore = firebase.firestore();
 export default function App() {
   return (
     <div className="App">
